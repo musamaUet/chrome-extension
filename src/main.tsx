072de4cce92332886ffe17357c21dc4162e1cbc3
musamauet <usamaitians.gcuf@gmail.com>
2024-12-08 13:11:39 +0500

    update fixations

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Toaster } from 'react-hot-toast';
 import './index.css';
+import React from 'react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
